408ca5430cf870afbfc614083f127fea1b9407f2
gredd182 <134130676+gredd182@users.noreply.github.com>
2023-08-03 22:45:21 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,14 +3,35 @@ provider "aws" {
   region = "ap-south-1" # Replace with your desired region
 }
 
-# Create an EC2 instance
+# Find an encrypted AMI ID in your desired region.
+data "aws_ami" "encrypted_ami" {
+  most_recent = true
+
+  filter {
+    name   = "root-device-type"
+    values = ["ebs"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["amazon"] # Replace with the appropriate AMI owner if necessary.
+}
+
+# Create an EC2 instance using the encrypted AMI for the root block device.
 resource "aws_instance" "example" {
-  ami           = "ami-0f5ee92e2d63afc18" # Replace with your desired AMI ID
+  ami           = data.aws_ami.encrypted_ami.id
   instance_type = "t2.micro" # Replace with your desired instance type
-  key_name      = "terraform"
+  key_name      = "my-keypair" # Replace with the name of your key pair
 
   tags = {
     Name = "MyEC2Instance"
   }
 }
 
+# Output the public IP address of the EC2 instance
+output "public_ip" {
+  value = aws_instance.example.public_ip
+}
