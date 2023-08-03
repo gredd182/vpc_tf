76f66b4260828d7852383eddece8ff6e7faf0e4d
gredd182 <134130676+gredd182@users.noreply.github.com>
2023-08-03 22:29:27 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,9 +1,14 @@
 variable "region" {
   description = "AWS region to deploy resources"
-  default     = "ap-south-1" # Replace with your desired region
+  default     = "us-east-1" # Replace with your desired region
 }
 
-variable "availability_zone" {
-  description = "AWS availability zone for the public subnet"
-  default     = "ap-south-1a" # Replace with your desired availability zone
+variable "instance_type" {
+  description = "EC2 instance type"
+  default     = "t2.micro" # Replace with your desired instance type
+}
+
+variable "ami" {
+  description = "AMI ID for the EC2 instance"
+  default     = "ami-0c55b159cbfafe1f0" # Replace with your desired AMI ID
 }
