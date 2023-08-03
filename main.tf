20de158d1688e044d81bd814e53e2ec50af61a88
gredd182 <134130676+gredd182@users.noreply.github.com>
2023-08-03 22:35:21 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -5,7 +5,7 @@ provider "aws" {
 
 # Create an EC2 instance
 resource "aws_instance" "example" {
-  ami           = "ami-0c55b159cbfafe1f0" # Replace with your desired AMI ID
+  ami           = "ami-0f5ee92e2d63afc18" # Replace with your desired AMI ID
   instance_type = "t2.micro" # Replace with your desired instance type
   key_name      = "terraform"
 
