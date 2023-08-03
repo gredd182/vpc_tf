f3f1615295e7ace05f8550433065e2fbaa6b0b7b
gredd182 <134130676+gredd182@users.noreply.github.com>
2023-08-03 22:28:42 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,50 +3,17 @@ provider "aws" {
   region = "us-east-1" # Replace with your desired region
 }
 
-# Create a VPC
-resource "aws_vpc" "main" {
-  cidr_block = "10.0.0.0/16"
+# Create an EC2 instance
+resource "aws_instance" "example" {
+  ami           = "ami-0c55b159cbfafe1f0" # Replace with your desired AMI ID
+  instance_type = "t2.micro" # Replace with your desired instance type
+
   tags = {
-    Name = "MyVPC"
+    Name = "MyEC2Instance"
   }
 }
 
-# Create a public subnet
-resource "aws_subnet" "public_subnet" {
-  vpc_id            = aws_vpc.main.id
-  cidr_block        = "10.0.1.0/24"
-  availability_zone = "us-east-1a" # Replace with your desired availability zone
-  tags = {
-    Name = "Public Subnet"
-  }
-}
-
-# Create an internet gateway
-resource "aws_internet_gateway" "gw" {
-  vpc_id = aws_vpc.main.id
-}
-
-# Attach the internet gateway to the VPC
-resource "aws_vpc_attachment" "gw_attachment" {
-  vpc_id       = aws_vpc.main.id
-  internet_gateway_id = aws_internet_gateway.gw.id
-}
-
-# Create a security group to allow incoming SSH traffic
-resource "aws_security_group" "allow_ssh" {
-  name_prefix = "allow_ssh"
-  description = "Allow SSH inbound traffic"
-  vpc_id      = aws_vpc.main.id
-
-  ingress {
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-}
-
-# Output the VPC ID
-output "vpc_id" {
-  value = aws_vpc.main.id
+# Output the public IP address of the EC2 instance
+output "public_ip" {
+  value = aws_instance.example.public_ip
 }
